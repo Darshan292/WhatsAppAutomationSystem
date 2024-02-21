a36fbaabde412c88897a09183ed74b2353910d26
Darshan U Shetty <96349479+Darshan292@users.noreply.github.com>
2024-02-21 22:43:18 +0530

    Update speech.js

--- speech.js
+++ speech.js
@@ -13,14 +13,12 @@ async function speech(lang, topic, word_num){
         const page = await browser.newPage();
         await page.goto(websiteUrl, { waitUntil: 'domcontentloaded' });
         await page.waitForSelector(targettopic);
-        console.log(topic+" within "+word_num+" words");
         await page.type(targettopic,topic+" within "+word_num+" words");
         await page.waitForSelector(langselect);
         await page.select(langselect,lang)
         await page.click(button);
         await page.waitForTimeout(6000);
         await page.waitForSelector(targetdiv, { visible: true});
-        // const ptag = document.querySelector(targetdiv)
         speechcontent = await page.$eval(targetdiv, (element) => element.innerHTML);
     }catch(error){
         console.error('Error:', error.message);
